6427685d82ed59fa41c6d521a4b33e9c4e7d60b7
luhur65 <dharmabakti1202@gmail.com>
2020-04-03 12:01:05 +0700

    Change to the english lang

--- Game.py
+++ Game.py
@@ -1,294 +1,281 @@
-# Ini File Game.py yg berisi function setiap tingkat game kuis
+# this file is contain a function for every mode game in the file main.py
 
-# mengimport module operand
+# import module operand
 from operator import *
 
-# Mengimport module/library random
+# import module random
 import random
 
-# Mode Game Mudah
-def modeMudah(data):
+# Easy Game Mode
+def modeEasy(data):
 
-    # Jumlah Soal 
-    jmlSoal = 0
+    # Count Total Quiz default value is 0
+    countQuiz = 0
 
-    # Skor Sekarang
+    # Current Skor default value is 0
     currentSkor = 0
 
-    # No Soal
-    intSoal = 0
+    # Number Quiz
+    numberQuiz = 0
 
-    for jmlSoal in range(1,11):
+    for countQuiz in range(1,11):
 
-        # Mencetak no soal 
-        currentSoal = 1
-        intSoal += currentSoal
+        # Mencetak no Quiz 
+        currentQuiz = 1
+        numberQuiz += currentQuiz
 
-        # Angka Random
-        angka1 = random.randint(1,25)
-        angka2 = random.randint(1,25)
+        # Number Random to make a quiz 
+        randomNo1 = random.randint(1,25)
+        randomNo2 = random.randint(1,25)
         
-        # Soal Mudah ( penjumlahan )
-        soal = add(angka1, angka2)
-        print('{}. Hasil Dari {} + {} ??'.format(intSoal, angka1, angka2))
+        # Quiz Mudah ( penjumlahan )
+        Quiz = add(randomNo1, randomNo2)
+        print('{}. What The Result From {} + {} ??'.format(numberQuiz, randomNo1, randomNo2))
 
-        # print(soal)
+        # print(Quiz)
         answer = input('Answer : ')
 
         try:
-            if int(answer) == soal:
-                # Skor Jawaban yg Benar
-                jwbnBenar = 10
+            if int(answer) == Quiz:
+                # the skor if the answer is true
+                scoreForRightAnswer = 10
 
-                currentSkor += jwbnBenar
-                print('\n---Jawaban Anda Benar ----')
-                print('Skor Saya: {}\n'.format(currentSkor))
+                currentSkor += scoreForRightAnswer
+                print('\n--- Well Done , Your Answer is Correct ----')
+                print('My Score: {}\n'.format(currentSkor))
             
-            elif int(answer) != soal:
+            elif int(answer) != Quiz:
                 # Memberikan Koreksi Jwbn Yg benar
-                print('\n---- Jawaban Anda Salah ---- ')
-                print('Jawaban Yang Benar : {}\n'.format(soal))
+                print('\n---- Unfortunetly , Your Answer is Wrong ---- ')
+                print('The Right Answer is : {}\n'.format(Quiz))
         
         except Exception as err:
-            err = '\nJawaban Tidak Dikenali!\n'
+            err = '\nYour Answer Can\'t be Accepted!\n'
             print(err)
 
-    # Kategori Nilai Akhir
-    kategoriNilai(currentSkor, data)
+    # category to know how great smart you ??
+    gradeForFinish(currentSkor, data)
 
 
-# Mode Game Sedang
-def modeSedang(data):
-    jmlSoal = 0
+# Medium Mode game
+def modeMedium(data):
+    countQuiz = 0
     currentSkor = 0
-    intSoal = 0
+    numberQuiz = 0
 
-    # Nyawa / Kesempatan Player Salah Menjawab
+    # Life to player
     life = 3
 
-    print('Anda Mempunyai : 3 Nyawa , Jika Salah Menjawab Nyawa Anda Berkurang!!\n')
+    print('You Only have 3 life , if you wrong to answer your life will lost -1 !!\n')
 
-    for jmlSoal in range(1,21):
-        # Mencetak no soal 
-        currentSoal = 1
-        intSoal += currentSoal
+    for countQuiz in range(1,21):
+        # print no Quiz 
+        currentQuiz = 1
+        numberQuiz += currentQuiz
 
-        # Angka Random
-        angka1 = random.randint(1,50)
-        angka2 = random.randint(1,50)
+        # No. Random
+        randomNo1 = random.randint(1,50)
+        randomNo2 = random.randint(1,50)
 
         # operasi matematika
-        listMtk = random.randint(1,2)
+        shuffleQuiz = random.randint(1,2)
         
-        if listMtk == 1:
-            soal = add(angka1, angka2)
-            print('{}. Hasil Dari {} + {} ??'.format(intSoal, angka1, angka2))
+        if shuffleQuiz == 1:
+            Quiz = add(randomNo1, randomNo2)
+            print('{}. What The Result From {} + {} ??'.format(numberQuiz, randomNo1, randomNo2))
 
-        elif listMtk == 2:
-            soal = sub(angka1, angka2)
-            print('{}. Hasil Dari {} - {} ??'.format(intSoal, angka1, angka2))
+        elif shuffleQuiz == 2:
+            Quiz = sub(randomNo1, randomNo2)
+            print('{}. What The Result From {} - {} ??'.format(numberQuiz, randomNo1, randomNo2))
 
          # Jawaban User
         answer = input('Answer : ')
 
         try:
-            if int(answer) == soal:
-                # Skor Jawaban yg Benar
-                jwbnBenar = 5
+            if int(answer) == Quiz:
+                # score to right answer
+                scoreForRightAnswer = 5
 
-                currentSkor += jwbnBenar
-                print('\n---Jawaban Anda Benar ---')
-                print('Skor Saya : {}\n'.format(currentSkor))
+                currentSkor += scoreForRightAnswer
+                print('\n--- Well Done , Your Answer is Correct ---')
+                print('My Score : {}\n'.format(currentSkor))
             
-            elif int(answer) != soal:
-                # Skor Jawaban Yg Salah 
-                # Dikurangi
-                jwbnSalah = 3
-                currentSkor -= jwbnSalah
+            elif int(answer) != Quiz:
+                # score to worng answer
+                # will be substract 3
+                substractScore = 3
+                currentSkor -= substractScore
 
-                # Nyawa Akan Berkurang Jika Salah Menjawab Pertanyaan
+                # you life will be lost -1 if you are wrong to answer
                 life -= 1
 
-                print('\n---- Jawaban Anda Salah ----')
-                print('Jawaban Yang Benar : {}'.format(soal))
-                print('Skor Saya : {}'.format(currentSkor))
+                print('\n---- Unfortunetly , Your Answer is Wrong ----')
+                print('The Right Answer is : {}'.format(Quiz))
+                print('My Score : {}'.format(currentSkor))
                 print('Nyawa Anda : {} life\n'.format(life))
 
-                # Permainan Berakhir Jika Nyawa / Kesempatan Salah Menjawab Sudah Habis
+                # game over if all your life is lost
                 if life == 0:
-                    print('Game Over , Anda Sudah Salah 3x Menjawab!!\n')
+                    print('Game Over , You have been 3 times wrong to answer the quiz !!\n')
                     break
             
         except Exception as err:
-            err = '\nJawaban Tidak Dikenali!\n'
+            err = '\nYour Answer Can\'t be Accepted!\n'
             print(err)
 
     # Kategori Nilai Akhir
-    kategoriNilai(currentSkor, data)
+    gradeForFinish(currentSkor, data)
 
 
-# Mode Game Sulit
-def modeSulit(data):
-    jmlSoal = 0
+# hard Game
+def modeHard(data):
+    countQuiz = 0
     currentSkor = 0
-    intSoal = 0
+    numberQuiz = 0
 
-    # Nyawa / Kesempatan Player Salah Menjawab
     life = 3
 
-    print('Anda Mempunyai : 3 Nyawa , Jika Salah Menjawab Nyawa Anda Berkurang!!\n')
+    print('You Only have 3 life , if you wrong to answer your life will lost -1 !!\n')
 
-    for jmlSoal in range(1,26):
-        # Mencetak no soal 
-        currentSoal = 1
-        intSoal += currentSoal
+    for countQuiz in range(1,26):
+        currentQuiz = 1
+        numberQuiz += currentQuiz
 
-        # Angka Random Utama untuk Penjumlahan, Pengurangan, Perkalian
-        angka1 = random.randint(1,100)
-        angka2 = random.randint(1,100)
+        randomNo1 = random.randint(1,100)
+        randomNo2 = random.randint(1,100)
 
-        # Angka Random Tambahan untuk Operasi Campuran
-        angka3 = random.randint(1,50)
-        angka4 = random.randint(1,25)
+        randomNo3 = random.randint(1,50)
+        randomNo4 = random.randint(1, 25)
+        
+        shuffleQuiz = random.randint(1,4)
 
-        # Operasi Matematika untuk menentukan soal operasi mana yg keluar
-        listMtk = random.randint(1,4)
+        if shuffleQuiz == 1:
+            Quiz = (sub(randomNo1, randomNo3)) * add(randomNo4, randomNo2)
+            print('{}. What The Result From ( {} - {} ) *  {} + {} '.format(numberQuiz, randomNo1, randomNo3, randomNo4, randomNo2))
 
-        if listMtk == 1:
-            soal = (sub(angka1, angka3)) * add(angka4, angka2)
-            print('{}. Hasil Dari ( {} - {} ) *  {} + {} '.format(intSoal, angka1, angka3, angka4, angka2))
+        elif shuffleQuiz == 2:
+            Quiz = sub(randomNo1, randomNo2)
+            print('{}. What The Result From {} - {} ??'.format(numberQuiz, randomNo1, randomNo2))
 
-        elif listMtk == 2:
-            soal = sub(angka1, angka2)
-            print('{}. Hasil Dari {} - {} ??'.format(intSoal, angka1, angka2))
+        elif shuffleQuiz == 3:
+            Quiz = mul(randomNo1, randomNo2)
+            print('{}. What The Result From {} * {} ??'.format(numberQuiz, randomNo1, randomNo2))
 
-        elif listMtk == 3:
-            soal = mul(angka1, angka2)
-            print('{}. Hasil Dari {} * {} ??'.format(intSoal, angka1, angka2))
+        elif shuffleQuiz == 4:
+            Quiz = mul(randomNo4, randomNo1) - ( add(randomNo2, randomNo3) )
+            print('{}. What The Result From  {} * {}  - ( {} + {} )'.format(numberQuiz, randomNo4, randomNo1, randomNo2, randomNo3))
 
-        elif listMtk == 4:
-            # Soal Operasi Matematika Campuran
-            soal = mul(angka4, angka1) - ( add(angka2, angka3) )
-            print('{}. Hasil Dari  {} * {}  - ( {} + {} )'.format(intSoal, angka4, angka1, angka2, angka3))
-
-        # jawaban user
-        # print(soal)
         answer = input('Answer: ')
 
         try:
-            if int(answer) == soal:
-            # Skor Jawaban yg Benar
-                jwbnBenar = 4
+            if int(answer) == Quiz:
+                scoreForRightAnswer = 4
 
-                currentSkor += jwbnBenar
-                print('\nSkor Saya: {}'.format(currentSkor))
-                print('---Jawaban Anda Benar ----\n')
+                currentSkor += scoreForRightAnswer
+                print('\nMy Score: {}'.format(currentSkor))
+                print('--- Well Done , Your Answer is Correct ----\n')
             
-            elif int(answer) != soal:
-                # Skor Jawaban Yg Salah 
-                # Dikurangi
-                jwbnSalah = 3
-                currentSkor -= jwbnSalah
+            elif int(answer) != Quiz:
+                substractScore = 3
+                currentSkor -= substractScore
 
-                # Nyawa Akan Berkurang Jika Salah Menjawab Pertanyaan
+                # life will be lost
                 life -= 1
 
-                print('\n---- Jawaban Anda Salah ----')
-                print('Jawaban Yang Benar : {}'.format(soal))
-                print('Skor Saya: {}'.format(currentSkor))
+                print('\n---- Unfortunetly , Your Answer is Wrong ----')
+                print('The Right Answer is : {}'.format(Quiz))
+                print('My Score: {}'.format(currentSkor))
                 print('Nyawa Anda : {} life\n'.format(life))
 
-                # Permainan Berakhir Jika Nyawa / Kesempatan Salah Menjawab Sudah Habis
+                # game over
                 if life == 0:
                     print('Game Over , Anda Sudah Salah 3x Menjawab!!\n')
                     break
             
         except Exception as err:
-            err = '\nJawaban Tidak Dikenali!\n'
+            err = '\nYour Answer Can\'t be Accepted!\n'
             print(err)
     
-    # Kategori Nilai Akhir
-    kategoriNilai(currentSkor,data)
+    gradeForFinish(currentSkor,data)
 
 
 def advancedMode(pemain):
-    # nyawa user == 3
+    # usr life  == 3
     life = 3
     # Total Skor
-    skor= 0
-    # no soal
-    noSoal = 0
+    score= 0
+    # no Quiz
+    noQuiz = 0
 
     for i in range(1,500):
 
-        noSoal += 1
+        noQuiz += 1
 
-        # angka random buat soal kuis
-        angka1 = random.randint(1, 20)
-        angka2 = random.randint(21, 40)
-        angka3 = random.randint(41, 60)
-        angka4 = random.randint(61, 100)
+        # angka random buat Quiz kuis
+        randomNo1 = random.randint(1, 20)
+        randomNo2 = random.randint(21, 40)
+        randomNo3 = random.randint(41, 60)
+        randomNo4 = random.randint(61, 100)
 
-        # random acak buat nentuin soal yg muncul
-        pilihSoal = random.randint(1, 5)
+        shuffleQuiz = random.randint(1, 5)
 
-        # Membuat pilihSoal
-        if pilihSoal == 1:
-            soal = add(angka1, angka2)
-            print('{}. Hasil Dari {} + {} ??'.format(noSoal, angka1, angka2))
-        elif pilihSoal == 2:
-            soal = sub(angka1, angka2)
-            print('{}. Hasil Dari {} - {} ??'.format(noSoal, angka1, angka2))
-        elif pilihSoal == 3:
-            soal = mul(angka3, angka4)
-            print('{}. Hasil Dari {} * {} ??'.format(noSoal, angka3, angka4))
-        elif pilihSoal == 4:
-            soal = angka4 * ( angka3 + angka1 - angka2) * angka4
-            print('{}. Hasil Dari {} * ( {} + {} - {} ) * {} ??'.format(noSoal, angka4, angka3, angka1, angka2, angka4))
+        # shuffleQuiz
+        if shuffleQuiz == 1:
+            Quiz = add(randomNo1, randomNo2)
+            print('{}. What The Result From {} + {} ??'.format(noQuiz, randomNo1, randomNo2))
+        elif shuffleQuiz == 2:
+            Quiz = sub(randomNo1, randomNo2)
+            print('{}. What The Result From {} - {} ??'.format(noQuiz, randomNo1, randomNo2))
+        elif shuffleQuiz == 3:
+            Quiz = mul(randomNo3, randomNo4)
+            print('{}. What The Result From {} * {} ??'.format(noQuiz, randomNo3, randomNo4))
+        elif shuffleQuiz == 4:
+            Quiz = randomNo4 * ( randomNo3 + randomNo1 - randomNo2) * randomNo4
+            print('{}. What The Result From {} * ( {} + {} - {} ) * {} ??'.format(noQuiz, randomNo4, randomNo3, randomNo1, randomNo2, randomNo4))
 
-        # jawaban user
+        # user answer
         try:
-            print(soal)
-            answer = int(input('Jawaban Anda :\t'))
+            print(Quiz)
+            answer = int(input('Your Answer is :\t'))
 
-            # jika jawaban benar 
-            if answer == soal:
-                skor += 10
+            # if user answer is true
+            if answer == Quiz:
+                score += 10
 
-                print('\n---Jawaban Benar!---')
-                print('Skor Mu = {}'.format(skor))
-                print('Nyawa Anda = {} Life \n'.format(life))
+                print('\n---Your Answer is True !---')
+                print('Your Score = {}'.format(score))
+                print('Your Life = {} Life \n'.format(life))
 
-                # jika user bisa mencapai soal ke 500
-                if noSoal == 500 and life == 3:
+                # if user reach score 500 
+                if noQuiz == 500:
                     print('\n-----Congratulation For You!----')
                     print('--------------------------------')
-                    print('Skor Kamu Berjumlah => {}'.format(skor))
+                    print('You Have Finished , The Mission With Total Score => {}'.format(score))
                     break
 
-            # jika jawaban salah
-            elif answer != soal:
-                skor -= 5
+            # if wrong
+            elif answer != Quiz:
+                score -= 5
                 life -= 1
 
-                print('\n---Jawaban Salah!---')
-                print('Jawaban Benarnya = {}'.format(soal))
-                print('Nyawa Anda = {} Life\n'.format(life))
+                print('\n---Wrong Answer!---')
+                print('The Right Answer is = {}'.format(Quiz))
+                print('Your Life = {} Life\n'.format(life))
 
-                # cek nyawa pemain
+                # check user life
                 if life == 0:
-                    print('Permainan Habis!, Nyawa Anda Tidak Ada Lagi!')
-                    print('Skor yg Anda Peroleh = {} Point\n'.format(skor))
+                    print('Game Over!, Your Life is 0!')
+                    print('Score that you have = {} Point\n'.format(skor))
                     break
 
         except Exception as err:
             print(err)
     
 
-def kategoriNilai(jmlSkor, pemain):
+def gradeForFinish(totalScore, playerName):
     # List Ucapan 
     # kategori > 80
-    kategoriBaik = [
+    goodCategory = [
         'Kamu Orang Yg Pintar, Pertahankan!',
         'Wah , Kamu Hebat Sekali !!',
         'Jawaban Kamu Diatas rata-rata',
@@ -300,7 +287,7 @@ def kategoriNilai(jmlSkor, pemain):
         'Gak Ada Yang Bisa Ngalahin Kamu !!'
     ]
     # kategori 50 > nilai < 75
-    kategoriCukup = [
+    enoughCategory = [
         'Oke Lumayan Untuk Kamu',
         'Jangan Langsung Berbangga Diri yah Ini Belum Seberapa',
         'Aku Yakin Kamu bisa lebih baik dari ini',
@@ -310,7 +297,7 @@ def kategoriNilai(jmlSkor, pemain):
         'Terus Belajar & raih Kategori A'
     ]
     # kategori < 45
-    kategoriBuruk = [
+    badCategory = [
         'Yah , Semangat Terus Deh Buat Kamu',
         'Ayo Kamu Pasti Bisa',
         'Mungkin Kamu Masih Belum terbiasa ya !!',
@@ -321,22 +308,22 @@ def kategoriNilai(jmlSkor, pemain):
     ]
 
     # Pengacakan pesan kategori
-    pesanBaik = random.choice(kategoriBaik)
-    pesanCukup = random.choice(kategoriCukup)
-    pesanBuruk = random.choice(kategoriBuruk)
+    goodQuotes = random.choice(goodCategory)
+    enoughQuotes = random.choice(enoughCategory)
+    badQuotes = random.choice(badCategory)
 
     # Total Jumlah Skor yg Diperoleh , 
-    # Kategory Tingkat Kemampuan dalam Mengerjakan Soal
-    if jmlSkor <= 45:
-        print('Skor Anda :', jmlSkor, ' ( Kategori : C )')
-        print('Quotes Untuk Anda : \n', pesanBuruk, pemain)
+    # Kategory Tingkat Kemampuan dalam Mengerjakan Quiz
+    if totalScore <= 45:
+        print('Your Final Score is :', totalScore, ' ( Category : C )')
+        print('Quotes To You : \n', badQuotes, playerName)
        
-    elif jmlSkor >= 50 and jmlSkor <= 75:
-        print('Skor Anda :', jmlSkor, ' ( Kategori : B )')
-        print('Quotes Untuk Anda : \n', pesanCukup, pemain)
+    elif totalScore >= 50 and totalScore <= 75:
+        print('Your Final Score is :', totalScore, ' ( Category : B )')
+        print('Quotes To You : \n', enoughQuotes, playerName)
        
-    elif jmlSkor >= 80:
-        print('Skor Anda :', jmlSkor, ' ( Kategori : A )')
-        print('Quotes Untuk Anda : \n', pesanBaik, pemain)
+    elif totalScore >= 80:
+        print('Your Final Score is :', totalScore, ' ( Category : A )')
+        print('Quotes To You : \n', goodQuotes, playerName)
        
 
--- Main.py
+++ Main.py
@@ -4,60 +4,60 @@ import Game
 # Data Mode Awal Game
 modeGame = 0
 
-print('  Welcome To Game Kuis Matematika  '.center(80,'-'))
+print(' Welcome To Math Quiz '.center(80,'-'))
 print(' By : Dharma Bakti Situmorang '.center(80))
 
 # Data Nama Pemain
-print('\nSiapa Nama Kamu??')
+print('\nWhat\'s your name ,boy ??')
 playerName = input() # atau kamu bisa langsung mengisikan namamu disini dengan menggantikan input() dangan nama mu
 
 while modeGame != 9:
 
     print('\n')
-    print(' Selamat Bermain !! '.center(80, '-'))
+    print(' Le\'s Go Play The Quiz !! '.center(80, '-'))
     print('-'.center(80, '-'))
-    print('Silakan Pilih Tingkat Kesulitan Permainan ??')
+    print('Choose the category dificulty ??')
 
     # Membuat List Mode Game
-    print('(1) Mudah')
-    print('(2) Sedang')
-    print('(3) Sulit \n')
+    print('(1) Easy')
+    print('(2) Medium')
+    print('(3) Hard \n')
 
     print('[6] Game Mode Advanced')
-    print('[7] Ganti Nama Pemain')
-    print('[9] Keluar')
+    print('[7] Change Name')
+    print('[9] Quit Game')
 
     try:
         # user memilih 
-        modeGame = int(input('\nPilihan Anda :'))
+        modeGame = int(input('\nYour Choice is :'))
 
     except ValueError:
-        print('Masukkan Angka Saja!!')
-        print('Pilih Hanya Kategori Diatas Saja\n')
+        print('Enter Only The Number!!')
+        print('Choose Only The Category in above\n')
     
     if modeGame == 1:
         # User Meminta Mode Mudah
-        print('\nAnda Memilih Tingkat Mudah, Jawab Semua Soal')
-        Game.modeMudah(playerName)
+        print('\nYou Choose The Easy Mode')
+        Game.modeEasy(playerName)
 
     elif modeGame == 2:
         # User Meminta Mode Sedang
-        print('\nAnda Memilih Tingkat Sedang/Normal, Jawab Semua Soal')
-        Game.modeSedang(playerName)
+        print('\nYou Choose Medium Mode')
+        Game.modeMedium(playerName)
 
     elif modeGame == 3:
         # User Meminta Mode Sulit
-        print('\nAnda Memilih Tingkat Sulit, Jawab Semua Soal')
-        Game.modeSulit(playerName)
+        print('\nYou Choose Hard Mode')
+        Game.modeHard(playerName)
 
     elif modeGame == 6:
         # user ingin bermain game mode campuran / advanced
-        print('\nDi Game Mode Yang Satu Ini Anda Ditantang Untuk Memdapatkan nilai sebanyak-banyaknya!!')
+        print('\nIn This Game You Will Challenge to get a lot of point that you can get!!')
         Game.advancedMode(playerName)
 
     elif modeGame == 7:
         # user ingin mengubah namanya 
-        print('Siapa Nama Kamu Sekarang ??')
+        print('What\'s your name now , boy ??')
         playerName = input()
 
 print('Good Bye!!!')
